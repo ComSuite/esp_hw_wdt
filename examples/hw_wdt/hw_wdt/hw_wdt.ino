48999dd9e29a9da761a2f2206902945ca4e7098c
Alexander Epstine <alex@comsuite.co.il>
2017-11-29 09:51:25 +0200

    Add files via upload

---
+++ examples/hw_wdt/hw_wdt/hw_wdt.ino
@@ -0,0 +1,22 @@
+#include "esp_hw_wdt.h"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  wdt_disable(); //disable sowtwarte wdt
+  esp_hw_wdt_disable(); //disable hardware wdt
+
+  for (uint64_t i = 0; i < 100000; i++){
+      Serial.println("wait");
+  }
+
+  esp_hw_wdt_enable(); //enable hardware wdt
+  wdt_enable(1000); //enablw software wdt
+}
+
+void loop() {
+  Serial.println("loop");
+  // put your main code here, to run repeatedly:
+  delay(100);
+}
